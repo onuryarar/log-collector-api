2ad5dbd378e3a2c52b836b2910da9fc0e98a4786
Onur Yarar <onur.yarar@detaysoft.com>
2020-10-07 14:28:36 +0300

    URL parametresi düzenlendi

--- server.js
+++ server.js
@@ -6,16 +6,17 @@ const argv = require('yargs')
         choices: ['dev', 'prod']
     }).argv;
 
-if (argv.type == 'dev') process.env.URL = process.env.DEV_URL;
+const port = process.env.PORT || 3010;
+
+if (argv.type == 'dev') process.env.URL = process.env.DEV_URL + ':' + port;
 if (argv.type == 'prod') process.env.URL = process.env.LIVE_URL;
 
 const url = process.env.URL;
-const port = process.env.PORT || 3010;
 
 const server = http.createServer(app);
 
 server.on('listening', () => {
-    console.log(`server is running at → \x1b[32m${url}:${port} \x1b[31m`);
+    console.log(`server is running at → \x1b[32m${url} \x1b[31m`);
 });
 
 server.listen(port);
