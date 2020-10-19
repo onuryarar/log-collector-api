138999358a9f298d0b799b196ef084bc0f5a9409
Onur Yarar <onur.yarar@detaysoft.com>
2020-10-19 09:53:28 +0300

    origin url değişti

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 const corsOptions = {
-    origin: 'http://localhost:3005',
+    origin: 'http://localhost:3006',
     optionsSuccessStatus: 200
 }
 
