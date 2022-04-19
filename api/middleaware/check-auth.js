b3e5f09e8416b511104a9cf956445718b17c4cab
Onur Yarar <onurr.yarar@hotmail.com>
2022-04-19 17:19:50 +0300

    authorization check bug fix

--- api/middleaware/check-auth.js
+++ api/middleaware/check-auth.js
@@ -2,7 +2,7 @@ const jwt = require('jsonwebtoken');
 
 module.exports = (req, res, next) => {
     try {
-        const [method, token] = req.header.authorization.split(' ');
+        const [method, token] = req.headers.authorization.split(' ');
         const decoded = jwt.verify(token, process.env.JWT_KEY);
         req.user = decoded;
         next();
